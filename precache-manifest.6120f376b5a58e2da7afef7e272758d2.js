66163448a1d9747375ef580d4d2c0ea0cf1482ae
William Smith <williamsmith@Services-MacBook-Pro.local>
2020-08-21 08:12:15 +1000

    Updates

---
+++ precache-manifest.6120f376b5a58e2da7afef7e272758d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fa802d4a86f34a7573dc1ea655d0c9",
+    "url": "/nbaratings-web-react/index.html"
+  },
+  {
+    "revision": "06a5f52495f53f6de321",
+    "url": "/nbaratings-web-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4a7e8ba58dd091e6a53a",
+    "url": "/nbaratings-web-react/static/js/2.648ba25d.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/nbaratings-web-react/static/js/2.648ba25d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a5f52495f53f6de321",
+    "url": "/nbaratings-web-react/static/js/main.b820567f.chunk.js"
+  },
+  {
+    "revision": "91916d880e5b65aeafac",
+    "url": "/nbaratings-web-react/static/js/runtime-main.71778feb.js"
+  }
+]);
